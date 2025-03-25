3193e9ff6b81d8004a32ca1883b29defb036eb9d
mukeshkumar098 <mukeshkemar567@gmail.com>
2025-03-25 11:35:13 +0530

    check

--- student-backend/config/db.js
+++ student-backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
