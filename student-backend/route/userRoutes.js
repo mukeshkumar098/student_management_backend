f2024557031f77afdc51f4094f8f882434ffc760
mukeshkumar098 <mukeshkemar567@gmail.com>
2025-03-25 12:22:24 +0530

    Delete student-backend directory

--- student-backend/route/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controller/Authcontroller.js";
-const userRoute=express.Router();
-
-
-userRoute.post("/register",register)
-userRoute.post("/login",login)
-
-export {userRoute}
